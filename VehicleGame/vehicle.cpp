17d8d4f337b7fb01549e1f116cbe8e50cb50cf88
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-07-26 07:55:55 -0700

    Refactoring

--- VehicleGame/vehicle.cpp
+++ VehicleGame/vehicle.cpp
@@ -2,13 +2,12 @@
 
 void Vehicle::Output()
 {
-	double fuelPercentage =
-		m_fuelCapacity != 0 ? m_fuel / m_fuelCapacity * 100 : 0;
+	float fuelPercentage = CalculateFuelPercentage();
 
 	printf("\n-------------------------------------\n");
 	printf("Name: %s\n", m_name.c_str());
 	printf("Distance traveled: %g miles\n", m_distanceDriven);
-	printf("Fuel Efficiency: %g miles/g\n", m_fuelConsumption);
+	printf("Fuel Efficiency: %g miles/gal\n", m_fuelConsumption);
 	printf("Fuel: %g gal/%g gal   %g%% full\n", m_fuel, m_fuelCapacity, fuelPercentage);
 	printf("-------------------------------------\n\n");
 }
@@ -23,8 +22,9 @@ Vehicle::VEHICLE_DRIVE_RESULT Vehicle::Drive(double distance)
 
 	// convert m_fuelConsumption from miles/gal to gallons/mi
 	double gallonsPerMile = 1 / m_fuelConsumption;
+	double fuelConsumed = distance * gallonsPerMile;
 
-	m_fuel -= distance * gallonsPerMile;
+	m_fuel -= fuelConsumed;
 	m_distanceDriven += distance;
 
 	return VEHICLE_DRIVE_RESULT::Success;
@@ -35,12 +35,27 @@ bool Vehicle::NotEnoughFuel(double distance)
 	return (distance * 1 / m_fuelConsumption) > m_fuel;
 }
 
+float Vehicle::CalculateFuelPercentage()
+{
+	if (m_fuelCapacity == 0 || m_fuel == 0) return 0.00;
+
+	float fuelPercentage = m_fuel / m_fuelCapacity * 100;
+
+	return fuelPercentage;
+}
+
+bool Vehicle::IsTooMuchFuel(int amount)
+{
+	return (m_fuel + amount) > m_fuelCapacity;
+}
+
+
 bool Vehicle::AddFuel(int amount)
 {
-	if ((m_fuel + amount) > m_fuelCapacity)
-		return false;
+	if (IsTooMuchFuel(amount)) return false;
 
 	m_fuel += amount;
 
 	return true;
 }
+
--- VehicleGame/vehicle.h
+++ VehicleGame/vehicle.h
@@ -23,7 +23,6 @@ public:
 	};
 
 protected:
-
 	double m_fuel = 0;
 	double m_distanceDriven = 0;
 	double m_fuelCapacity = 0;
@@ -40,13 +39,16 @@ public:
 
 	Vehicle(double fuelCapacity = 0,
 		double fuelConsumption = 0) :
-		m_fuelCapacity( fuelCapacity ),
-		m_fuelConsumption( fuelConsumption ) {}
+		m_fuelCapacity(fuelCapacity),
+		m_fuelConsumption(fuelConsumption) {};
 
 	void Output();
 	bool AddFuel(int amount);
-
-	bool NotEnoughFuel(double distance);
 	VEHICLE_DRIVE_RESULT Drive(double distance);
 	virtual void SetRandomEncounters() = 0;
+
+private:
+	bool IsTooMuchFuel(int amount);
+	bool NotEnoughFuel(double distance);
+	float CalculateFuelPercentage();
 };
